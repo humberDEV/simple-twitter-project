2239f111d38986128ce424f7866a4924ce471a79
Humberto García Arellano <humberbrawl@gmail.com>
2023-08-25 22:36:33 +0200

    Changed to map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import './App.css'
+import { useState } from 'react'
 import { TwitterCard } from './components/TwitterCard'
 
+const usersMock = [
+    {
+      "username": "humberDEV",
+      "name": "Humberto Garía"
+    },
+    {
+      "username": "enriquedlc",
+      "name": "Enrique De La Concepción"
+    },
+    {
+      "username": "midudev",
+      "name": "Miguel Ángel Durán"
+    },
+    {
+      "username": "pino",
+      "name": "El Tio"
+    },
+    {
+      "username": "ericoso",
+      "name": "Eric Rodriguez"
+    }
+  ]
+
+import './App.css'
+
 export function App () {
+
+    const [users, setUsers] = useState(usersMock) // => el useState recibe com primer parámetro el estado inicial
+
+    console.log( setUsers )
+
     return(
         <div className="App">
-            <TwitterCard userName={'humberDEV'} name={'Humberto García'} isFollowing/>
-            <TwitterCard userName={'enriquedlc'} name={'Enrique de la Concepción'} isFollowing/>
-            <TwitterCard userName={'midudev'} name={'Miguel Ángel Durán'} isFollowing={false}/>
+            {
+                users.map((user, index) => 
+                    (
+                        <TwitterCard key={index} name={user.name} userName={user.username} />
+                    )
+                )
+            }
+
+            {/* <CreateUser/> */}
+
         </div>
     )
 }
